22add83c2ca1df66c004a6a9b4fd8670bf60fc35
zhudongya123@126.com <zhudongya123@126.com>
2017-07-02 22:32:31 +0800

    整理逻辑

--- Wechat_chatroom_helper/app/src/main/java/com/zdy/project/wechat_chatroom_helper/HookLogic.java
+++ Wechat_chatroom_helper/app/src/main/java/com/zdy/project/wechat_chatroom_helper/HookLogic.java
@@ -1,8 +1,15 @@
 package com.zdy.project.wechat_chatroom_helper;
 
 import android.content.Context;
+import android.provider.Telephony;
 import android.util.AttributeSet;
 import android.util.Log;
+import android.util.SparseArray;
+import android.view.View;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
@@ -16,19 +23,33 @@ import static com.zdy.project.wechat_chatroom_helper.Constants.WECHAT_PACKAGE_NA
  */
 
 public class HookLogic implements IXposedHookLoadPackage {
+
+
+   String Conversation_List_View =".ui.conversation.g";
+
     @Override
     public void handleLoadPackage(XC_LoadPackage.LoadPackageParam loadPackageParam) throws Throwable {
         if (!loadPackageParam.packageName.equals(WECHAT_PACKAGE_NAME)) return;
 
 
-        XposedHelpers.findAndHookConstructor(WECHAT_PACKAGE_NAME + ".ui.mogic.WxViewPager", loadPackageParam
-                        .classLoader,
-                Context.class, AttributeSet.class, new XC_MethodHook() {
+        XposedHelpers.findAndHookMethod(WECHAT_PACKAGE_NAME + ".ui.conversation.g", loadPackageParam
+                        .classLoader, "a",
+                "com.tencent.mm.ui.e.b", SparseArray.class, HashMap.class, new XC_MethodHook() {
 
                     @Override
                     protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                        AttributeSet attributeSet = (AttributeSet) param.args[1];
-                        Log.v("wechat_chatroom_helper", attributeSet.toString());
+                        SparseArray sparseArray = (SparseArray) param.args[1];
+                        Log.v(".ui.conversation.g.a", sparseArray.toString() + "");
+
+                        HashMap<String, Object> hashMap = (HashMap) param.args[2];
+                        for (Map.Entry<String, Object> stringObjectEntry : hashMap.entrySet()) {
+
+                            String key = stringObjectEntry.getKey();
+                            Object value = stringObjectEntry.getValue();
+
+
+                            Log.v(".ui.conversation.g.a", "key = " + key + ", value = " + value);
+                        }
                     }
 
                     @Override
@@ -36,5 +57,43 @@ public class HookLogic implements IXposedHookLoadPackage {
                     }
                 });
 
+
+        /**
+         * Conversation_List_View
+
+        com.tencent.mm.storage.ad
+
+为实体类model
+         */
+
+        XposedHelpers.findAndHookMethod(WECHAT_PACKAGE_NAME + ".ui.conversation.g", loadPackageParam
+                        .classLoader, "b",
+                "com.tencent.mm.storage.ad", int.class, boolean.class, new XC_MethodHook() {
+
+                    @Override
+                    protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                        Object ad = param.args[0];
+
+                        Object hdc = XposedHelpers.getObjectField(ad, "hdc");
+
+                        Object tww = XposedHelpers.getObjectField(hdc, "tww");
+
+
+                        Log.v("storage.ad", "field_username = " + XposedHelpers.getObjectField(ad, "field_username").toString());
+                        Log.v("storage.ad", "field_content = " + XposedHelpers.getObjectField(ad, "field_content").toString());
+                        Log.v("storage.ad", "field_msgCount = " + XposedHelpers.getObjectField(ad, "field_msgCount").toString());
+                        Log.v("storage.ad", "field_digest = " + XposedHelpers.getIntField(ad, "field_digest")+"");
+                        Log.v("storage.ad", "field_conversationTime = " + new Date(XposedHelpers.getLongField(ad, "field_conversationTime")*1000).toString());
+
+
+                        XposedHelpers.setObjectField(param.args[0], "field_content","喫屎咧梁非凡");
+                    }
+
+
+
+                    @Override
+                    protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                    }
+                });
     }
 }
