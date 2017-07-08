ba1cde077eabdeb104e18e777548b5bbd925486d
zhudongya123@126.com <zhudongya123@126.com>
2017-07-08 16:07:05 +0800

    列表页现在已经支持修改

--- Wechat_chatroom_helper/app/src/main/java/com/zdy/project/wechat_chatroom_helper/HookLogic.java
+++ Wechat_chatroom_helper/app/src/main/java/com/zdy/project/wechat_chatroom_helper/HookLogic.java
@@ -6,13 +6,19 @@ import android.util.AttributeSet;
 import android.util.Log;
 import android.util.SparseArray;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.zdy.project.wechat_chatroom_helper.model.MessageEntity;
+
+import java.lang.reflect.Field;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
 
@@ -24,8 +30,10 @@ import static com.zdy.project.wechat_chatroom_helper.Constants.WECHAT_PACKAGE_NA
 
 public class HookLogic implements IXposedHookLoadPackage {
 
-
-   String Conversation_List_View =".ui.conversation.g";
+    /**
+     * com.tencent.mm.ui.e 应当是ListView的 adapter 的父类
+     */
+    String Conversation_List_View = ".ui.conversation.g";
 
     @Override
     public void handleLoadPackage(XC_LoadPackage.LoadPackageParam loadPackageParam) throws Throwable {
@@ -60,10 +68,12 @@ public class HookLogic implements IXposedHookLoadPackage {
 
         /**
          * Conversation_List_View
-
-        com.tencent.mm.storage.ad
-
-为实体类model
+         *
+         * com.tencent.mm.storage.ad 为实体类model
+         *
+         * 其中conversation.g类中，方法b 为返回消息内容（nickname），参数表 b(com.tencent.mm.storage.ad paramad, int paramInt, boolean paramBoolean)
+         *
+         *
          */
 
         XposedHelpers.findAndHookMethod(WECHAT_PACKAGE_NAME + ".ui.conversation.g", loadPackageParam
@@ -82,18 +92,54 @@ public class HookLogic implements IXposedHookLoadPackage {
                         Log.v("storage.ad", "field_username = " + XposedHelpers.getObjectField(ad, "field_username").toString());
                         Log.v("storage.ad", "field_content = " + XposedHelpers.getObjectField(ad, "field_content").toString());
                         Log.v("storage.ad", "field_msgCount = " + XposedHelpers.getObjectField(ad, "field_msgCount").toString());
-                        Log.v("storage.ad", "field_digest = " + XposedHelpers.getIntField(ad, "field_digest")+"");
-                        Log.v("storage.ad", "field_conversationTime = " + new Date(XposedHelpers.getLongField(ad, "field_conversationTime")*1000).toString());
+                        Log.v("storage.ad", "field_conversationTime = " + new Date(XposedHelpers.getLongField(ad, "field_conversationTime") * 1000).toString());
 
-
-                        XposedHelpers.setObjectField(param.args[0], "field_content","喫屎咧梁非凡");
+                        XposedHelpers.setObjectField(param.args[0], "field_digest", "喫屎咧梁非凡");
                     }
-
 
 
                     @Override
                     protected void afterHookedMethod(MethodHookParam param) throws Throwable {
                     }
                 });
+
+
+        XposedHelpers.findAndHookMethod(WECHAT_PACKAGE_NAME + Conversation_List_View, loadPackageParam
+                        .classLoader, "getView",
+                int.class, View.class, ViewGroup.class, new XC_MethodHook() {
+
+                    @Override
+                    protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                    }
+
+
+                    @Override
+                    protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+
+
+                        int position = (int) param.args[0];
+                        View itemView = (View) param.args[1];
+                        ViewGroup parent = (ViewGroup) param.args[2];
+
+
+                        XposedBridge.log("XposedBridge, getView itemView = " + itemView.toString());
+
+                        Object value = XposedHelpers.callMethod(param.thisObject, "ev", position);
+
+                        MessageEntity entity=new MessageEntity(value);
+
+                        XposedBridge.log("XposedBridge, getView MessageEntity = " + entity.toString());
+
+
+                        Object viewHolder = itemView.getTag();
+
+                        Object textView = XposedHelpers.getObjectField(viewHolder, "usj");
+
+                        XposedHelpers.callMethod(textView,"setText","王倩是傻逼");
+
+                    }
+                });
     }
+
+
 }
--- Wechat_chatroom_helper/app/src/main/java/com/zdy/project/wechat_chatroom_helper/MainActivity.java
+++ Wechat_chatroom_helper/app/src/main/java/com/zdy/project/wechat_chatroom_helper/MainActivity.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
