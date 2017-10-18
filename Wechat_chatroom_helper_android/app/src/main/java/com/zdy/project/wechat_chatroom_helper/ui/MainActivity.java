f6f74e9835ab0498467e4d1058faa6de11bc2fe2
zhudongya123@126.com <zhudongya123@126.com>
2017-10-18 22:54:51 +0800

    装逼就学kotlin

--- Wechat_chatroom_helper_android/app/src/main/java/com/zdy/project/wechat_chatroom_helper/ui/MainActivity.java
+++ Wechat_chatroom_helper_android/app/src/main/java/com/zdy/project/wechat_chatroom_helper/ui/MainActivity.java
@@ -226,33 +226,28 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private int getWechatVersionCode() {
-
-        PackageManager packageManager = getPackageManager();
-        List<PackageInfo> packageInfoList = packageManager.getInstalledPackages(0);
+        List<PackageInfo> packageInfoList = getPackageManager().getInstalledPackages(0);
         try {
             for (PackageInfo packageInfo : packageInfoList) {
-
                 if (packageInfo.packageName.equals("com.tencent.mm")) {
                     if (packageInfo.versionName.equals("6.5.14"))
-                        return 1120;
+                        return 1101;
                     return packageInfo.versionCode;
                 }
             }
-            return 1060;
+            return -1;
 
         } catch (Throwable e) {
             e.printStackTrace();
-            return 1060;
+            return -1;
         }
     }
 
     private int getHelperVersionCode(Context context) {
         PackageManager packageManager = context.getPackageManager();
-        PackageInfo packageInfo;
         int versionCode = 0;
         try {
-            packageInfo = packageManager.getPackageInfo(context.getPackageName(), 0);
-            versionCode = packageInfo.versionCode;
+            versionCode = packageManager.getPackageInfo(context.getPackageName(), 0).versionCode;
         } catch (PackageManager.NameNotFoundException e) {
             e.printStackTrace();
         }
@@ -268,10 +263,9 @@ public class MainActivity extends AppCompatActivity {
             addPreferencesFromResource(R.xml.pref_setting);
 
             final EditTextPreference toolbarColor = ((EditTextPreference) findPreference("toolbar_color"));
-            setToolbarColor(toolbarColor);
-
-
             final SwitchPreference play_version = (SwitchPreference) findPreference("play_version");
+
+            setToolbarColor(toolbarColor);
             setCheckPlayVersion(play_version);
         }
 
