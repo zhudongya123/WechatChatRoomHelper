006a57a8c2b38c772b3eee2812d607c7aaab5495
zhudongya123@126.com <zhudongya123@126.com>
2018-03-26 20:00:08 +0800

    improve kt  file

--- Wechat_chatroom_helper_android/app/src/main/kotlin/com/zdy/project/wechat_chatroom_helper/ui/helper/uisetting/SettingFragment.kt
+++ Wechat_chatroom_helper_android/app/src/main/kotlin/com/zdy/project/wechat_chatroom_helper/ui/helper/uisetting/SettingFragment.kt
@@ -23,6 +23,9 @@ class SettingFragment : Fragment() {
     private lateinit var contentLayout: LinearLayout
 
     private var titles = arrayOf("助手ToolBar颜色", "助手背景颜色", "会话列表标题颜色", "会话列表内容颜色", "会话列表时间颜色", "会话列表分割线颜色")
+    private var types = arrayOf(ChooseColorDialogHelper.ColorType.Toolbar, ChooseColorDialogHelper.ColorType.Helper, ChooseColorDialogHelper.ColorType.Nickname,
+            ChooseColorDialogHelper.ColorType.Content, ChooseColorDialogHelper.ColorType.Time, ChooseColorDialogHelper.ColorType.Divider)
+
 
     override fun onAttach(context: Context?) {
         super.onAttach(context)
@@ -31,24 +34,15 @@ class SettingFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
-        contentLayout = LinearLayout(thisActivity)
-        contentLayout.orientation = LinearLayout.VERTICAL
+        contentLayout = LinearLayout(thisActivity).apply { orientation = LinearLayout.VERTICAL }
 
-        for (i in 0 until titles.size) {
-            val itemView = LayoutInflater.from(thisActivity).inflate(R.layout.layout_color_setting_item, contentLayout, false)
-            itemView.findViewById<TextView>(android.R.id.text1).text = titles[i]
-            contentLayout.addView(itemView)
-
-            itemView.setOnClickListener {
-                when (i) {
-                    0 -> ChooseColorDialogHelper.getDialog(thisActivity, ChooseColorDialogHelper.ColorType.Toolbar).show()
-                    1 -> ChooseColorDialogHelper.getDialog(thisActivity, ChooseColorDialogHelper.ColorType.Helper).show()
-                    2 -> ChooseColorDialogHelper.getDialog(thisActivity, ChooseColorDialogHelper.ColorType.Nickname).show()
-                    3 -> ChooseColorDialogHelper.getDialog(thisActivity, ChooseColorDialogHelper.ColorType.Content).show()
-                    4 -> ChooseColorDialogHelper.getDialog(thisActivity, ChooseColorDialogHelper.ColorType.Time).show()
-                    5 -> ChooseColorDialogHelper.getDialog(thisActivity, ChooseColorDialogHelper.ColorType.Divider).show()
-                }
-            }
+        repeat(titles.size) { index ->
+            contentLayout.addView(LayoutInflater.from(thisActivity)
+                    .inflate(R.layout.layout_color_setting_item, contentLayout, false)
+                    .also {
+                        it.findViewById<TextView>(android.R.id.text1).text = titles[index]
+                        it.setOnClickListener { ChooseColorDialogHelper.getDialog(thisActivity, types[index]).show() }
+                    })
         }
         return contentLayout
 
@@ -56,9 +50,7 @@ class SettingFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-
         settingViewHolder = UISettingActivity.obtainSettingViewModel(thisActivity)
-
         setupSettingColor()
     }
 
@@ -71,34 +63,30 @@ class SettingFragment : Fragment() {
     private fun setupSettingColor() {
         settingViewHolder.toolbarColor.observe(thisActivity, Observer<String> {
             setItemColor(0, it)
-       //     (thisActivity.supportFragmentManager.findFragmentByTag(PreviewFragment::class.java.simpleName) as PreviewFragment).notifyUIToChangeColor()
         })
         settingViewHolder.helperColor.observe(thisActivity, Observer<String> {
             setItemColor(1, it)
-       //     (thisActivity.supportFragmentManager.findFragmentByTag(PreviewFragment::class.java.simpleName) as PreviewFragment).notifyUIToChangeColor()
         })
         settingViewHolder.nicknameColor.observe(thisActivity, Observer<String> {
             setItemColor(2, it)
-      //      (thisActivity.supportFragmentManager.findFragmentByTag(PreviewFragment::class.java.simpleName) as PreviewFragment).notifyUIToChangeColor()
         })
         settingViewHolder.contentColor.observe(thisActivity, Observer<String> {
             setItemColor(3, it)
-       //     (thisActivity.supportFragmentManager.findFragmentByTag(PreviewFragment::class.java.simpleName) as PreviewFragment).notifyUIToChangeColor()
         })
         settingViewHolder.timeColor.observe(thisActivity, Observer<String> {
             setItemColor(4, it)
-      //      (thisActivity.supportFragmentManager.findFragmentByTag(PreviewFragment::class.java.simpleName) as PreviewFragment).notifyUIToChangeColor()
         })
         settingViewHolder.dividerColor.observe(thisActivity, Observer<String> {
             setItemColor(5, it)
-      //      (thisActivity.supportFragmentManager.findFragmentByTag(PreviewFragment::class.java.simpleName) as PreviewFragment).notifyUIToChangeColor()
         })
     }
 
     private fun setItemColor(index: Int, value: String?) {
-        val text = contentLayout.getChildAt(index).findViewById<TextView>(android.R.id.text2)
-        text.text = value
-        text.setTextColor(ColorUtils.getColorInt(value.toString()))
+        contentLayout.getChildAt(index).findViewById<TextView>(android.R.id.text2)
+                .also { view ->
+                    view.text = value
+                    view.setTextColor(ColorUtils.getColorInt(value.toString()))
+                }
     }
 
 
