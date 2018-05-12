29cdb52d8da04d7c854c1cfba6d71fff8ee484c9
zhudongya123@126.com <zhudongya123@126.com>
2018-05-12 22:52:20 +0800

    😭

--- Wechat_chatroom_helper_android/app/src/main/kotlin/com/zdy/project/wechat_chatroom_helper/plugins/main/adapter/Classes.kt
+++ Wechat_chatroom_helper_android/app/src/main/kotlin/com/zdy/project/wechat_chatroom_helper/plugins/main/adapter/Classes.kt
@@ -4,19 +4,26 @@ import android.database.Cursor
 import android.widget.ImageView
 import com.gh0u1l5.wechatmagician.spellbook.C
 import com.gh0u1l5.wechatmagician.spellbook.WechatGlobal
+import com.gh0u1l5.wechatmagician.spellbook.mirror.mm.ui.Classes.MMBaseAdapter
 import com.gh0u1l5.wechatmagician.spellbook.mirror.mm.ui.conversation.Classes
+import com.gh0u1l5.wechatmagician.spellbook.mirror.mm.ui.conversation.Classes.ConversationWithCacheAdapter
 import com.gh0u1l5.wechatmagician.spellbook.util.ReflectionUtil
 import com.zdy.project.wechat_chatroom_helper.plugins.PluginEntry
 import de.robv.android.xposed.XposedHelpers
+import java.lang.reflect.Field
+import java.lang.reflect.ParameterizedType
+import java.text.FieldPosition
 
 object Classes {
 
-    val ConversationWithAppBrandListView: Class<*> by WechatGlobal.wxLazy("ConversationWithAppBrandListView") {
-        ReflectionUtil.findClassIfExists("${WechatGlobal.wxPackageName}.ui.conversation.ConversationWithAppBrandListView", WechatGlobal.wxLoader)
+
+    private val WechatClasses by WechatGlobal.wxLazy("WechatClasses") {
+        WechatGlobal.wxClasses!!.filter { it.contains(WechatGlobal.wxPackageName) }.map { XposedHelpers.findClass(it, PluginEntry.classloader) }
     }
 
-    val WechatClasses by WechatGlobal.wxLazy("WechatClasses") {
-        WechatGlobal.wxClasses!!.filter { it.contains(WechatGlobal.wxPackageName) }.map { XposedHelpers.findClass(it, PluginEntry.classloader) }
+
+    val ConversationWithAppBrandListView: Class<*> by WechatGlobal.wxLazy("ConversationWithAppBrandListView") {
+        ReflectionUtil.findClassIfExists("${WechatGlobal.wxPackageName}.ui.conversation.ConversationWithAppBrandListView", WechatGlobal.wxLoader)
     }
 
     val ClassesByCursor by WechatGlobal.wxLazy("ClassesByCursor") {
@@ -52,17 +59,16 @@ object Classes {
 
     }
 
+    val SetConversationString = Classes.ConversationWithCacheAdapter.declaredMethods
+            .filter { !it.isAccessible }
+            .filter { it.returnType == CharSequence::class.java }
+            .firstOrNull { it.parameterTypes.size == 1 }!!
+
     fun getConversationTimeString(adapter: Any, conversationTime: Long): CharSequence {
 
-        val method = Classes.ConversationWithCacheAdapter.declaredMethods
-                .filter { !it.isAccessible }
-                .filter { it.returnType == CharSequence::class.java }
-                .firstOrNull { it.parameterTypes.size == 1 }
-
-        method?.let {
+        SetConversationString.let {
 
             val aeClass = XposedHelpers.findClass(it.parameterTypes[0].name, PluginEntry.classloader)
-
             val constructor = aeClass.constructors.filter { it.parameterTypes.size == 1 }.firstOrNull { it.parameterTypes[0] == String::class.java }
 
             constructor?.let {
@@ -71,16 +77,37 @@ object Classes {
                 aeClass.getField("field_status").set(obj, 0)
                 aeClass.getField("field_conversationTime").set(obj, conversationTime)
 
-                return XposedHelpers.callMethod(adapter, method.name, obj) as CharSequence
+                return XposedHelpers.callMethod(adapter, SetConversationString.name, obj) as CharSequence
             }
         }
         return ""
     }
 
 
-    fun getConversationAvatar(field_username: String, imageView: ImageView) {
-        XposedHelpers.callStaticMethod(SetAvatarClass, SetAvatarMethod, imageView, field_username)
+    fun getConversationAvatar(field_username: String, imageView: ImageView) =
+            XposedHelpers.callStaticMethod(SetAvatarClass, SetAvatarMethod, imageView, field_username)!!
 
+
+    fun getConversationContent(adapter: Any, bean :Any, position: Int) {
+
+
+        val parameterizedType = ConversationWithCacheAdapter.genericSuperclass as ParameterizedType
+        val typeArguments = parameterizedType.actualTypeArguments
+
+        val paramAE = (typeArguments[1] as Class<*>)
+        var paramD = ConversationWithCacheAdapter.declaredClasses.first { it.fields.map { it.name }.contains("nickName") }
+
+
+        val getContentMethod = ConversationWithCacheAdapter.methods
+                .filter { it.parameterTypes.size == 3 }
+                .first {
+                    it.parameterTypes[0].simpleName == paramAE.simpleName &&
+                            it.parameterTypes[1].simpleName == Int::class.java.simpleName &&
+                            it.parameterTypes[2].simpleName == Boolean::class.java.simpleName
+                }
+
+
+//        XposedHelpers.callMethod(adapter,getContentMethod.name,bean,position,)
     }
 
 }
