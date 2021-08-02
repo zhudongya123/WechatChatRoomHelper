65fae244e56405d4e1f3473663b404db4bcce29f
Mr_Zdy <zhudongya123@126.com>
2021-08-02 21:11:48 +0800

    [FEAT] 适配新的引导流程ui库

--- Wechat_chatroom_helper_android/app/src/main/kotlin/com/zdy/project/wechat_chatroom_helper/helper/ui/config/ConfigActivity.kt
+++ Wechat_chatroom_helper_android/app/src/main/kotlin/com/zdy/project/wechat_chatroom_helper/helper/ui/config/ConfigActivity.kt
@@ -7,126 +7,123 @@ import android.support.annotation.IdRes
 import android.support.annotation.LayoutRes
 import android.support.annotation.StringRes
 import android.support.v4.content.ContextCompat
+import android.support.v7.app.AppCompatActivity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import com.android.setupwizardlib.SetupWizardLayout
+import com.android.setupwizardlib.view.NavigationBar
 import com.zdy.project.wechat_chatroom_helper.Constants
 import com.zdy.project.wechat_chatroom_helper.R
 import com.zdy.project.wechat_chatroom_helper.io.WechatJsonUtils
 import com.zdy.project.wechat_chatroom_helper.io.AppSaveInfo
 import manager.PermissionHelper
-import me.omico.base.activity.SetupWizardBaseActivity
-import ui.MainActivity
 
-class ConfigActivity : SetupWizardBaseActivity(), View.OnClickListener {
+class ConfigActivity : AppCompatActivity() {
 
-    private val PAGE_WELCOME = 0
-    private val PAGE_WRITE_AND_READ_FILE = 1
-    private val PAGE_WRITE_CONFIG = 2
+    enum class PageName {
+        WELCOME,
+        WRITE_AND_READ_FILE,
+        WRITE_CONFIG
+    }
 
-    private var setupStep: Int = 0
 
+    private var currentPage: PageName = PageName.WELCOME
+
+
+    private lateinit var setupWizardLayout: SetupWizardLayout
+    private lateinit var mContainerLayout: ViewGroup
     private lateinit var syncHandler: SyncHandler
-
-    override fun onClick(v: View) {
-        when (v.id) {
-            R.id.config_step2_button1 -> {
-                val check = PermissionHelper.check(this)
-                when (check) {
-                    PermissionHelper.ALLOW -> {
-                        initColorTextView(R.id.config_step2_text1, R.string.config_permission_success, R.color.right_color)
-                        setNavigationBarNextButtonEnabled(true)
-                    }
-                    PermissionHelper.ASK -> PermissionHelper.requestPermission(this)
-                    PermissionHelper.DENY -> {
-                        initColorTextView(R.id.config_step2_text1, R.string.config_permission_fail, R.color.error_color)
-                        PermissionHelper.gotoPermissionPage(this)
-                        setNavigationBarNextButtonEnabled(false)
-                    }
-                }
-            }
-            else -> {
-            }
-        }
-    }
-
-    override fun onWindowFocusChanged(hasFocus: Boolean) {
-        super.onWindowFocusChanged(hasFocus)
-
-        if (hasFocus) {
-            if (setupStep == PAGE_WRITE_AND_READ_FILE) {
-                val check = PermissionHelper.check(this)
-                when (check) {
-                    PermissionHelper.ALLOW -> {
-                        initColorTextView(R.id.config_step2_text1, R.string.config_permission_success, R.color.right_color)
-                        WechatJsonUtils.init(null)
-                        setNavigationBarNextButtonEnabled(true)
-                    }
-                    PermissionHelper.DENY -> {
-                        initColorTextView(R.id.config_step2_text1, R.string.config_permission_fail, R.color.error_color)
-                        setNavigationBarNextButtonEnabled(false)
-                    }
-                }
-            }
-        }
-    }
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_guide)
+        setupWizardLayout = findViewById(R.id.setup_wizard_layout)
+        mContainerLayout = findViewById(R.id.fragment_container)
 
-        setupStep = intent.getIntExtra("EXTRA_SETUP_STEP", 0)
+        setupPage()
+
+        setupWizardLayout.navigationBar.setNavigationBarListener(object : NavigationBar.NavigationBarListener {
+            override fun onNavigateBack() {
+                gotoPriorPage()
+            }
+
+            override fun onNavigateNext() {
+                gotoNextPage()
+            }
+        })
     }
 
-    override fun initLayout(viewGroup: ViewGroup) {
-        when (setupStep) {
+    fun gotoNextPage() {
+        when (currentPage) {
+            PageName.WELCOME -> {
+                currentPage = PageName.WRITE_AND_READ_FILE
+            }
+            PageName.WRITE_AND_READ_FILE -> {
+                currentPage = PageName.WRITE_CONFIG
+            }
+            PageName.WRITE_CONFIG -> {
+                finish()
+                return
+            }
+        }
+        setupPage()
+    }
 
-            PAGE_WELCOME -> {
-                navigationBar.backButton.visibility = View.GONE
-                initLayout(viewGroup, R.layout.config_layout_step_1, R.string.config_step1_title, true)
+    fun gotoPriorPage() {
+        when (currentPage) {
+            PageName.WELCOME -> {
+                finish()
+                return
             }
-            PAGE_WRITE_AND_READ_FILE -> {
-                initLayout(viewGroup, R.layout.config_layout_step_2, R.string.config_step2_title, false)
-                findViewById<View>(R.id.config_step2_button1).setOnClickListener(this)
+            PageName.WRITE_AND_READ_FILE -> {
+                currentPage = PageName.WELCOME
             }
-            PAGE_WRITE_CONFIG -> {
-                initLayout(viewGroup, R.layout.config_layout_step_3, R.string.config_step3_title, false)
+            PageName.WRITE_CONFIG -> {
+                currentPage = PageName.WRITE_AND_READ_FILE
+            }
+        }
+        setupPage()
+    }
+
+
+    fun setupPage() {
+        when (currentPage) {
+            PageName.WELCOME -> {
+                setupWizardLayout.navigationBar.backButton.visibility = View.GONE
+                initLayout(mContainerLayout, R.layout.config_layout_step_1, R.string.config_step1_title, true)
+            }
+            PageName.WRITE_AND_READ_FILE -> {
+                setupWizardLayout.navigationBar.backButton.visibility = View.VISIBLE
+                initLayout(mContainerLayout, R.layout.config_layout_step_2, R.string.config_step2_title, false)
+                checkPermission()
+            }
+            PageName.WRITE_CONFIG -> {
+                setupWizardLayout.navigationBar.backButton.visibility = View.VISIBLE
+                initLayout(mContainerLayout, R.layout.config_layout_step_3, R.string.config_step3_title, false)
                 parseApkClasses()
             }
         }
     }
 
 
-    override fun onNavigateBack() {
-        onBackPressed()
-    }
-
-    override fun onNavigateNext() {
-        when (setupStep) {
-            PAGE_WELCOME,
-            PAGE_WRITE_AND_READ_FILE -> {
-                intentNextStep()
-            }
-            PAGE_WRITE_CONFIG -> {
-                startActivity(Intent(this, MainActivity::class.java))
-            }
-        }
-    }
-
-    private fun intentNextStep() {
-        val intent = Intent(this, ConfigActivity::class.java)
-        intent.putExtra("EXTRA_SETUP_STEP", setupStep + 1)
-        startActivity(intent)
-    }
 
 
     private fun initLayout(viewGroup: ViewGroup, @LayoutRes layout: Int, @StringRes title: Int, nextButtonEnable: Boolean) {
+        viewGroup.removeAllViews()
         val inflate = LayoutInflater.from(this).inflate(layout, viewGroup, false)
         viewGroup.addView(inflate)
         setupWizardLayout.setHeaderText(title)
         setupWizardLayout.headerTextView.setTextColor(ContextCompat.getColor(this, R.color.white_100))
         setNavigationBarNextButtonEnabled(nextButtonEnable)
+    }
+
+
+    fun setNavigationBarNextButtonEnabled(nextButtonEnable: Boolean) {
+        setupWizardLayout.navigationBar.nextButton.isEnabled = nextButtonEnable
+
     }
 
     private fun initColorTextView(@IdRes id: Int, @StringRes text: Int, @ColorRes color: Int) {
@@ -135,9 +132,33 @@ class ConfigActivity : SetupWizardBaseActivity(), View.OnClickListener {
         textView.setTextColor(ContextCompat.getColor(this, color))
     }
 
+    private fun checkPermission() {
+        when (PermissionHelper.check(this)) {
+            PermissionHelper.ALLOW -> {
+                initColorTextView(R.id.config_step2_text1, R.string.config_permission_success, R.color.right_color)
+                WechatJsonUtils.init(null)
+                setNavigationBarNextButtonEnabled(true)
+            }
+            PermissionHelper.DENY -> {
+                initColorTextView(R.id.config_step2_text1, R.string.config_permission_fail, R.color.error_color)
+                setNavigationBarNextButtonEnabled(false)
+            }
+        }
+        findViewById<View>(R.id.config_step2_button1).setOnClickListener {
+            when (PermissionHelper.check(this)) {
+                PermissionHelper.ALLOW -> {
+                    initColorTextView(R.id.config_step2_text1, R.string.config_permission_success, R.color.right_color)
+                    setNavigationBarNextButtonEnabled(true)
+                }
+                PermissionHelper.ASK -> PermissionHelper.requestPermission(this)
+                PermissionHelper.DENY -> {
+                    initColorTextView(R.id.config_step2_text1, R.string.config_permission_fail, R.color.error_color)
+                    PermissionHelper.gotoPermissionPage(this)
+                    setNavigationBarNextButtonEnabled(false)
+                }
+            }
+        }
 
-    override fun setContentView(layoutResID: Int) {
-        super.setContentView(R.layout.activity_guide)
     }
 
     private fun parseApkClasses() {
@@ -160,8 +181,4 @@ class ConfigActivity : SetupWizardBaseActivity(), View.OnClickListener {
 
     }
 
-
-    fun setNavigationBarNextButtonEnabled2(result: Boolean) {
-        this.setNavigationBarNextButtonEnabled(result)
-    }
 }
